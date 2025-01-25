6c5927a4003d3f2d60c345096fa55e98be4fe6a7
Иванчик Павел Александрович (4091481) <ivanchik@dev.vtb.ru>
2025-01-25 14:44:13 +0300

    Initial commit

---
+++ src/main/java/by/java/InterviewsApi/InterviewsApiApplication.java
@@ -0,0 +1,13 @@
+package by.java.InterviewsApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewsApiApplication.class, args);
+	}
+
+}
